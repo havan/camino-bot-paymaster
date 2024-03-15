72f826d902ebd77d18a85badf0bc7a6d53f4b1a5
Ekrem Seren <ekrem.seren@gmail.com>
2024-03-15 23:27:15 +0300

    add recover pubkey to sign cheque script

--- scripts/signCheque.js
+++ scripts/signCheque.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { ethers } = require('ethers');
+const { ethers, computeAddress } = require('ethers');
 
 // Create a .env file by copying .env.example to .env and set the address and private key variables
 // 
@@ -15,6 +15,14 @@ const from = process.env.FROM_ADDRESS;
 const privateKey = process.env.PRIVATE_KEY;
 const wallet = new ethers.Wallet(privateKey);
 
+console.log("------ WALLET ------")
+console.log("PUBKEY\t\t:", wallet.signingKey.publicKey)
+console.log("COMPRESSED\t:", wallet.signingKey.compressedPublicKey)
+console.log("--------------------")
+
+console.log()
+
+
 const to = process.argv[2];
 const amount = process.argv[3];
 const nonce = process.argv[4];
@@ -24,18 +32,21 @@ const chequeHash = ethers.solidityPackedKeccak256(
     [from, to, amount, nonce]
 );
 
+console.log("------ CHEQUE ------")
 console.log('FROM\t:', from)
 console.log('TO\t:', to)
 console.log('AMOUNT\t:', amount)
 console.log('NONCE\t:', nonce)
 console.log('HASH\t:', chequeHash);
+console.log("--------------------")
 
 console.log()
-console.log('== SIGNATURES ==');
 
+console.log("------ SIGNATURES & COMPUTED PUBKEY & ADDRESS ------")
 async function signCheque(from, to, amount, nonce) {
     const signature = await wallet.signMessage(ethers.getBytes(chequeHash));
-    console.log('PREFIXED:\n', signature);
+    console.log('PREFIXED SIGNATURE:\n', signature);
+    console.log()
     return signature
 }
 
@@ -43,10 +54,20 @@ async function signCheque(from, to, amount, nonce) {
 async function signHashDirectly(from, to, amount, nonce) {
     const signature = await wallet.signingKey.sign(chequeHash);
     signatureSerialized = ethers.Signature.from(signature).serialized
-    console.log('NON-PREFIXED:\n', signatureSerialized);
+    console.log('NON-PREFIXED SIGNATURE (Use this with BotPayMaster):\n', signatureSerialized);
+    console.log()
     return signatureSerialized;
 }
 
-signCheque(from, to, amount, nonce)
+signCheque(from, to, amount, nonce).then(
+    nonPrefixedSignature => console.log("RECOVER PUBKEY (PREFIXED):\n", ethers.SigningKey.recoverPublicKey(chequeHash, nonPrefixedSignature), "\n")
+)
 
-signHashDirectly(from, to, amount, nonce)
+signHashDirectly(from, to, amount, nonce).then(
+    nonPrefixedSignature => {
+        pubkey = ethers.SigningKey.recoverPublicKey(chequeHash, nonPrefixedSignature);
+        console.log("RECOVER PUBKEY (NON-PREFIXED):\n", pubkey);
+        console.log();
+        console.log("COMPUTED ADDR (Should be same with 'from' address):\n", ethers.computeAddress(pubkey));
+    }
+)
